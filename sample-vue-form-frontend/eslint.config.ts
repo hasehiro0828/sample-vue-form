7d760297d773227a534773565eec7ef961bb0c80
Hiroto <hiroto.hasegawa.0828@gmail.com>
2025-10-07 09:41:41 +0900

    feat: add eslint and prettier

---
+++ sample-vue-form-frontend/eslint.config.ts
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended", "prettier"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+]);
