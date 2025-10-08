e4c99f50aa7470982b23d9a986dfd02d2afe20de
Hiroto <hiroto.hasegawa.0828@gmail.com>
2025-10-09 00:21:12 +0900

    feat: router.index.ts に NestForm を追加した

--- sample-vue-form-frontend/src/router/index.ts
+++ sample-vue-form-frontend/src/router/index.ts
@@ -1,9 +1,13 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/pages/Home.vue";
+import NestForm from "@/pages/NestForm/NestForm.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  { path: "/", name: "home", component: Home, children: [] },
+  { path: "/nest-form/:id", name: "nest-form", component: NestForm, children: [] },
+];
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
